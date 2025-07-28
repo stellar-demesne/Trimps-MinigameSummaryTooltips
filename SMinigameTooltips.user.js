49f3e06a840e7e8f9697754ef27524b9757cff45
stellar-demesne <54080611+stellar-demesne@users.noreply.github.com>
2025-07-28 02:16:32 -0700

    Create SMinigameTooltips.user.js

---
+++ SMinigameTooltips.user.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         MutationCounter
+// @namespace    https://github.com/stellar-demesne/Trimps-MinigameSummaryTooltips
+// @version      1.0
+// @updateURL    https://github.com/stellar-demesne/Trimps-MinigameSummaryTooltips/SMinigameTooltips.user.js
+// @description  Minigame Summary Tooltips
+// @author       StellarDemesne
+// @include      *trimps.github.io*
+// @include      *kongregate.com/games/GreenSatellite/trimps
+// @grant        none
+// ==/UserScript==
+var script = document.createElement('script');
+script.id = 'SMinigameTooltips';
+script.src = 'https://stellar-demesne.github.io/Trimps-MinigameSummaryTooltips/SMinigameTooltips.js';
+script.setAttribute('crossorigin', "anonymous");
+document.head.appendChild(script);
