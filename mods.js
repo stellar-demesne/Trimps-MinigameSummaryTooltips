1567d6bf6df33bf1e6e6226467f0f734a29736eb
stellar-demesne <54080611+stellar-demesne@users.noreply.github.com>
2025-07-28 02:17:55 -0700

    Create mods.js

---
+++ mods.js
@@ -0,0 +1,5 @@
+var script = document.createElement('script');
+script.id = 'SMinigameTooltips';
+script.src = 'https://stellar-demesne.github.io/Trimps-MinigameSummaryTooltips/SMinigameTooltips.js';
+script.setAttribute('crossorigin', "anonymous");
+document.head.appendChild(script);
